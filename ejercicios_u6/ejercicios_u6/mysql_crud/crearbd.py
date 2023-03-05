18192f49c3cc1fd9b0ada895e854d2962b1bcb07
Veronica <veronica.marcela.bar@gmail.com.ar>
2023-03-05 19:21:32 -0300

    ejercicios

--- ejercicios_u6/ejercicios_u6/mysql_crud/crearbd.py
+++ ejercicios_u6/ejercicios_u6/mysql_crud/crearbd.py
@@ -4,12 +4,10 @@ pip install pymysql
 pip install mysqlclient
 pip install mysql-connector-python
 """
-import mysql.connector
+import mysql.connector  # importar la librería
 
 mibase = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
+    host="localhost", user="root", password="", database="mis_libros"
 )
 
 micursor = mibase.cursor()
