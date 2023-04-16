ea6a23ee9b6fde7c4031689aefe860b4b2b5cb6a
Veronica <veronica.marcela.bar@gmail.com.ar>
2023-04-16 15:29:19 -0300

    primeros archivos de nivel intermedio

--- intermedio_m1_u1_ejercicios/e/main.py
+++ intermedio_m1_u1_ejercicios/e/main.py
@@ -1,6 +1,6 @@
 import modulo5
 
-print(list(modulo5.__dict__.keys()))
+print(list(modulo5.__dict__.keys())) #acá le estoy pidiendo que me mueste las claves q guarda en _dict y me lo muestra en consola
 
 print( modulo5.__dict__['__file__'])
 print( modulo5.__dict__['__name__'])
